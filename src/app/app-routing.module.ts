235bd4f076b270e38226df447f58af3b65da7627
Luis Miguel Ríos Pérez <luismrios1602@gmail.com>
2022-03-08 18:04:42 -0500

    * Se crea componente de inicio y seleccion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SelectionComponent } from './selection/selection.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component:HomeComponent
+  },
+  {
+    path: 'selection',
+    component:SelectionComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SelectionComponent } from './selection/selection.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectionComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
