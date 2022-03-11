a546631d953d996f11f30aff5e59b935d3a7c404
Luis Miguel Ríos Pérez <luismrios1602@gmail.com>
2022-03-11 03:12:33 -0500

    * Se le asigna la imagen de cada personaje a todas las cards del juego (seleccion, preview y batalla).

--- src/app/models/PersonajeModel.ts
+++ src/app/models/PersonajeModel.ts
@@ -9,7 +9,7 @@ export class PersonajeModel{
     danho!:Number; //El daño causado en su turno de ataque
     proteccion!:Number; //La protección adquirida en su turno de defensa
     victorias!:Number; //Las victorias que lleva en la partida (1, 2, o 3)
-    imagen!:Number;
+    image!:Number;
 
     constructor(){
         
