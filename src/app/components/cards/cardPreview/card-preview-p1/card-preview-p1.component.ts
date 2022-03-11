6dfa6cddf772eb33bb448edf64515a506a56be73
Luis Miguel Ríos Pérez <luismrios1602@gmail.com>
2022-03-11 00:54:19 -0500

    * Se muestra los personajes escogidos en la pantalla de loading

--- src/app/components/cards/cardPreview/card-preview-p1/card-preview-p1.component.ts
+++ src/app/components/cards/cardPreview/card-preview-p1/card-preview-p1.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PersonajeModel } from 'src/app/models/PersonajeModel';
+import { PersonajesService } from 'src/app/services/personajes/personajes.service';
 
 @Component({
   selector: 'app-card-preview-p1',
@@ -7,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardPreviewP1Component implements OnInit {
 
-  playername:String = "Player1";
-  constructor() { }
+  playername:String = "Player 1";
+  player1:PersonajeModel = new PersonajeModel();
+
+  constructor(private personajesService:PersonajesService) { }
 
   ngOnInit(): void {
+    this.player1 = this.personajesService.P1;
+    this.playername = this.player1.name;
   }
 
 }
--- src/app/components/cards/cardPreview/card-preview-p2/card-preview-p2.component.ts
+++ src/app/components/cards/cardPreview/card-preview-p2/card-preview-p2.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PersonajeModel } from 'src/app/models/PersonajeModel';
+import { PersonajesService } from 'src/app/services/personajes/personajes.service';
 
 @Component({
   selector: 'app-card-preview-p2',
@@ -7,11 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardPreviewP2Component implements OnInit {
 
-  playername:String = "CPU";
-  
-  constructor() { }
+  playername:String = "Player 2";
+  player2:PersonajeModel = new PersonajeModel();
+
+  constructor(private personajesService:PersonajesService) { }
 
   ngOnInit(): void {
+    this.player2 = this.personajesService.P2;
+    this.playername = this.player2.name;
   }
 
 }
